c04fad3b90a81e6a6d66d5592e6177054bc92f85
Eduardo Gonzalez <eglez@ast.cam.ac.uk>
2021-08-17 21:20:25 +0100

    Cleanup

--- setup.py
+++ setup.py
@@ -1,39 +1,40 @@
 from setuptools import find_packages, setup
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open('requirements.txt') as requirements_file:
+with open("requirements.txt") as requirements_file:
     requirements = [item.strip() for item in requirements_file.readlines()]
 
-with open('classifiers.txt') as classifiers_file:
+with open("classifiers.txt") as classifiers_file:
     classifiers = [item.strip() for item in classifiers_file.readlines()]
 
-setup_requirements = ['pytest-runner', 'flake8']
+setup_requirements = ["pytest-runner", "flake8"]
 
-test_requirements = ['coverage', 'pytest', 'pytest-cov', 'pytest-mock']
+test_requirements = ["coverage", "pytest", "pytest-cov", "pytest-mock"]
 
 setup(
-    author='Eduardo Gonzalez Solares',
-    author_email='eglez@ast.cam.ac.uk',
+    author="Eduardo Gonzalez Solares",
+    author_email="e.gonzalezsolares@gmail.com",
     classifiers=classifiers,
-    description='Owl Pipeline Server Framework',
-    entry_points={'console_scripts': ['owl-server=owl_server.cli:main', 'owl-migrate=owl_server.migrations.migrate:main']},
+    description="Owl Pipeline Server Framework",
+    entry_points={
+        "console_scripts": [
+            "owl-server=owl_server.cli:main",
+            "owl-migrate=owl_server.migrations.migrate:main",
+        ]
+    },
     install_requires=requirements,
-    license='GNU General Public License v3',
+    license="GNU General Public License v3",
     long_description=readme,
-    include_package_data=True,
-    keywords='owl, pipeline',
-    name='owl-server',
-    packages=find_packages(include=['owl_server*']),
-#    data_files=[
-#        ('conf', ['conf/pipeline.yaml', 'conf/scheduler.yaml', 'conf/log.yaml'])
-#    ],
+    keywords="owl, pipeline, dask, kubernetes, python",
+    name="owl-pipeline-server",
+    packages=find_packages(include=["owl_server*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://gitlab.ast.cam.ac.uk/imaxt/owl-pipeline-server',
-    version='0.4.0',
+    url="https://github.com/eddienko/owl-pipeline-server",
+    version="0.5.0",
     zip_safe=False,
-    python_requires='>=3.7',
+    python_requires=">=3.7",
 )
