59563c9501e5cd75bdf4b2b9bc5e9ba56fb2e136
Eduardo Gonzalez <eglez@ast.cam.ac.uk>
2021-08-18 08:21:43 +0100

    Add docker image files

---
+++ docker/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ ! -z "${RUN_DEVELOP:-}" ]]; then
+    pip install git+https://github.com/eddienko/owl-pipeline-server.git
+else
+    pip install owl-pipeline-server==0.6.0
+fi
+
+if [[ ! -z "${EXTRA_PIP_PACKAGES:-}" ]]; then
+    echo "EXTRA_PIP_PACKAGES environment variable found. Installing".
+    /opt/conda/bin/pip install $EXTRA_PIP_PACKAGES
+fi
+
+exec "$@"
