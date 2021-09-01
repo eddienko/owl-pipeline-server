f4b1c9bb1ac2d18cf34a298c79bb1f5506439e61
Eduardo Gonzalez <eglez@ast.cam.ac.uk>
2021-09-01 20:32:10 +0100

    Bump version: 0.8.1 → 0.8.2

--- docker/start.sh
+++ docker/start.sh
@@ -3,7 +3,7 @@
 if [[ ! -z "${RUN_DEVELOP:-}" ]]; then
     pip install git+https://github.com/eddienko/owl-pipeline-server.git@devel
 else
-    pip install owl-pipeline-server==0.8.1
+    pip install owl-pipeline-server==0.8.2
 fi
 
 if [[ ! -z "${EXTRA_CONDA_PACKAGES:-}" ]]; then
