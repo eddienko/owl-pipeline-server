c54fa80a5837a32b8f1e50a9c63d719611a7e285
Eduardo Gonzalez <eglez@ast.cam.ac.uk>
2021-10-07 10:35:31 +0100

    Correctly update pipeline status if it does not exist

--- owl_server/daemon/scheduler.py
+++ owl_server/daemon/scheduler.py
@@ -617,9 +617,11 @@ class Scheduler:
         await asyncio.sleep(0)
 
         self.logger.info("Updating pipeline %s - %s", uid, status)
-        data = {"status": status, "heartbeat": self.pipelines[uid].get("heartbeat")}
-        root = f"/api/pipeline/update/{uid}"
+        data = {"status": status}
+        with suppress(KeyError):
+            data["heartbeat"] = self.pipelines[uid]["heartbeat"]
 
+        root = f"/api/pipeline/update/{uid}"
         msg = await self._make_request(root, method="POST", data=data)
 
         if "detail" in msg:
