4a71b5b8be3cc289994a9c4506d5be811a0a892c
Eduardo Gonzalez Solares <eglez@ast.cam.ac.uk>
2022-11-02 14:53:12 +0000

    Fix API logging to database

--- owl_server/api/__init__.py
+++ owl_server/api/__init__.py
@@ -211,6 +211,10 @@ async def logger(payload: str = Body(...), request: Request = None):
     if payload["topic"] not in ["PIPELINE"]:
         return {}
 
+    level = payload.get("levelname"]
+    if level not in ["INFO", "WARNING", "ERROR", "CRITICAL"]:
+            return {}
+
     q = db.PipelineLogs.insert().values(
         jobid=int(payload["jobid"]),
         timestamp=dateutil.parser.parse(payload["asctime"]),
